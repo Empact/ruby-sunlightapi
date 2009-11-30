3a498ad5ec1864619041094aebdcc0015c3cdd3c
Ben Woosley <ben.woosley@gmail.com>
2009-11-30 11:03:12 -0500

    Simplify Lobbyist.search_by_name

--- lib/sunlight/lobbyist.rb
+++ lib/sunlight/lobbyist.rb
@@ -34,28 +34,14 @@ module Sunlight
 
       url = construct_url("lobbyists.search", :name => name, :threshold => threshold, :year => year)
       
-      if (results = get_json_data(url))
-        lobbyists = []
-        results["response"]["results"].each do |result|
-          if result
-            lobbyist = Lobbyist.new(result["result"]["lobbyist"])
-            fuzzy_score = result["result"]["score"]
-
-            if threshold.to_f < fuzzy_score.to_f
-              lobbyist.fuzzy_score = fuzzy_score.to_f
-              lobbyists << lobbyist
-            end
-          end
-        end
-
-        if lobbyists.empty?
-          nil
-        else
-          lobbyists
+      if (response = get_json_data(url))
+        lobbyists = response["response"]["results"].compact.map do |result|
+          Lobbyist.new(result["result"]["lobbyist"].merge("fuzzy_score" => result["result"]["score"].to_f))
+        end.select do |lobbyist|
+          lobbyist.fuzzy_score.to_f > threshold.to_f
         end
       
-      else
-        nil
+        lobbyists unless lobbyists.empty?
       end
     end # def self.search
   end # class Lobbyist
